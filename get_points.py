4d45755aba670376d21e84472067161bbf97fd8c
ToyeshJayaswal <44247501+ToyeshJayaswal@users.noreply.github.com>
2020-06-21 22:31:51 -0400

    Add files via upload

--- get_points.py
+++ get_points.py
@@ -8,11 +8,14 @@ def get_points(entry_file_path):
 	entrances = []
 	entry_file = open(entry_file_path)
 	entrance = []
-	points_str = entry_file.read()
-	points_str = points_str[2:-2]
-	point_arr = points_str.split("], [")
-	for coord_str in point_arr:
-		coords = coord_str.split(", ")
-		entrance.append([int(coords[0]),int(coords[1])])
-	entrances.append(Polygon(entrance))
+	lines = entry_file.readlines()
+	lines = set(lines)
+	for line in lines:
+		line = line[2:-3]
+		point_arr = line.split("], [")
+		for coord_str in point_arr:
+			coords = coord_str.split(", ")
+			entrance.append([int(coords[0]),int(coords[1])])
+		entrances.append(Polygon(entrance))
+		entrance = []
 	return entrances
